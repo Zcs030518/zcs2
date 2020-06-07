f11d378d50b5bd4de4e34d35f0ccfbf86d40785d
zcs <zcs123@163.com>
2020-06-07 22:07:26 +0800

    add files

---
+++ src/main/java/com/xupt/git/HelloGit.java
@@ -0,0 +1,12 @@
+package com.xupt.git;
+
+/**
+ * @author zcs
+ * @date 2020/6/7
+ **/
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello,Git!");
+    }
+}
